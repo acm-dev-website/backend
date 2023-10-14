230564beb64ffee6c914c6a57154fa36ea8fbfd6
MiniJ147 <147895450+JakePaulSWE@users.noreply.github.com>
2023-10-14 01:07:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //this allows for connect to cluster 0 in our database and then read and write from it
 //you might not be able to do it from home until I approve everyone to read and write
+<<<<<<< Updated upstream
 const dbURL = 'mongodb+srv://developer:<password>@cluster0.4ztfnxn.mongodb.net/?retryWrites=true&w=majority';
+=======
+const dbURL = 'mongodb+srv://developer:<password>@cluster0.4ztfnxn.mongodb.net/?retryWrites=true&w=majority'; //dont share this plz
+>>>>>>> Stashed changes
 mongoose.connect(dbURL);
 
 //sending index.html to the web broswer
