8891dbe93e52014d50883659a538ce39851e1715
thekightkeeper1 <ty.singh828@gmail.com>
2023-11-11 08:22:24 -0500

    Prev commit didn't work

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const {APIKey, PORT} = require("./Key.json");
 
+// Initilizing HTML parser
 app.set('view engine','ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+// Setting up schemas
 const eventSchema = {
   name: String,
   date: Date,
   description: String
 }
+const Event = mongoose.model('events', eventSchema);
 
+// Connecting to mongo
 const dbURL = `mongodb+srv://developer:${APIKey}@cluster0.4ztfnxn.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(dbURL);
 
@@ -24,25 +28,40 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/admin',(req,res)=>{
-    res.render('admin',{});
+    res.render('admin', {event: {name: ""}});
 })
 
 
-app.get('/admin/search',(req,res)=>{
-    let request = req.query.request;
+app.get('/admin/search', async (req, res) => {
+    let searchText = req.query.eventSearch;
+    console.log(`The search: ${searchText}`);
 
-    if(!request || request===""){
-        console.log("Not searching db");
-    }else{
-        console.log("Searching db");
+    // If user enters blank
+    if(!searchText || searchText===""){
+        res.send("error");
+        return;
     }
 
-    res.render('admin',{});
+    // If users searching string
+    const events = await Event.find({ name: searchText });
+    console.log(`Mongo found ${events}`);
+
+    // If the event is not found
+    if (events.length === 0) {
+        emptyEvent = { name: "Event not found :(" };
+        res.render("admin", { event: emptyEvent });
+        return;
+    }
+
+    // If the event is found then it just renders
+    
+    res.render("admin", {event: events[0]});
 })
 
 
+// Post requests
 app.post('/admin/', async (req, res) => {
-  const newEvent = new event({
+  const newEvent = new Event({
     name: req.body.name,
     date: req.body.date,
     description: req.body.description
@@ -57,6 +76,7 @@ app.post('/admin/', async (req, res) => {
 });
 
 
+// Running the server
 app.listen(PORT, ()=>{
     console.log("Server listening on port "+PORT);
     console.log(`http://localhost:${PORT}`)
