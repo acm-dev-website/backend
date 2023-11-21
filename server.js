a4574872562d6c278f006ed978398843f3f9496c
patrickijieh <reapful9234@gmail.com>
2023-11-20 21:10:03 -0500

    Add admin panel styling and form script

--- server.js
+++ server.js
@@ -79,11 +79,23 @@ app.get('/admin', cookieAuthCheck, (req,res)=>{
 })
 
 app.post('/admin/', cookieAuthCheck, async (req, res) => {
+  // Check to make sure all required fields have been filled out
+  const name = req.body.name.trim();
+  const date = req.body.date;
+  const desc = req.body.description.trim();
+  if (!req.body.name || !req.body.date || !req.body.description) {
+    res.status(400).redirect("/admin");
+    return;
+  }
+
+  // Create new event with field data
   const newEvent = new Event({
-    name: req.body.name,
-    date: req.body.date,
-    description: req.body.description
+    name: name,
+    date: date,
+    description: desc
   });
+
+  // Send to DB
   try {
     const e = await newEvent.save();
   }
