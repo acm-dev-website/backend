b4b1268043d067a1c01ace89bbb5c60bb0135705
zhiyan114 <zhiyan114@zhiyan114.com>
2023-11-26 23:54:00 -0500

    Remove fs pkg

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 
 const cookieParser = require('cookie-parser')
 const secureCookie = require('./secureCookie.js');
-const fs = require("fs");
 const dotenv = require('dotenv');
 dotenv.config();
 
