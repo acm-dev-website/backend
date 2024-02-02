11cb8b0a25e93b88eee897fa1926b6c2dacc859e
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-02-02 16:53:58 -0500

    img

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,40 @@
 /**
- * 
  * Validates the input in the event page form fields.
  * @param {Event} e
- *
  */
 function submitEvent(e) {
   e.preventDefault();
-  // Remove whitespace
+
   let name = document.getElementById("name").value.trim();
   let date = document.getElementById("date").value;
   let description = document.getElementById("description").value.trim();
-  
+  let imageInput = document.getElementById("image");
+  let imageFile = imageInput.files[0]; // Get the selected image file
+
   // Check for empty fields
   if (!name || !date || !description) {
-    alert("Please fill out all fields before submitting!");
-    return;
+      alert("Please fill out all fields before submitting!");
+      return;
   }
 
+  let formData = new FormData();
+  formData.append("name", name);
+  formData.append("date", date);
+  formData.append("description", description);
+  formData.append("image", imageFile);
+
   alert("Event Added!");
-  document.getElementById("event_form").submit();
+
+  submitFormData(formData);
+}
+
+function submitFormData(formData) {
+  fetch('/admin', {
+      method: 'POST',
+      body: formData
+  }).then(response => {
+      // Handle the response as needed
+  }).catch(error => {
+      console.error('Error:', error);
+  });
 }
