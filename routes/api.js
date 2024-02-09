06552fddc4df7e49f0b6b2bf088facd008c1eef5
Patrick Ijieh <ijiehpatrick@gmail.com>
2024-02-09 16:55:49 -0500

    Added queries

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,12 @@ const mongo_utils = require('../utils/mongo_utils');
 router.get('/fetch/events', async(req, res)=>{
     res.contentType = 'application/json';
 
+    const query = req.query;
     const db = mongo_utils.get_client().db();
     const collection = db.collection('events');
 
 
-    const result = await collection.find({},{projection:{_id:0}}).toArray();
+    const result = await collection.find(query,{projection:{_id:0}}).toArray();
 
     res.send({
         message: result
