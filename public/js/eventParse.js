d70d2b57c70599598becbd7d312d5904d907863e
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-03-08 17:00:35 -0500

    Image downloading and rendering

--- public/js/eventParse.js
+++ public/js/eventParse.js
@@ -13,13 +13,17 @@ fetch('api/fetch/events?type=gbm').then((res) => { return res.json() }).then((da
 		description.textContent = element.description;
 
 		// need to download image and append it as well - idk what images even exist.
+		// THIS WILL CRASH IF THE IMAGE DOES NOT EXIST
+		// THIS NEEDS TO BE HANDELED EITHER IN THE API ROUTE OR IN MONGO
+		let image = document.createElement("img");
+		image.src = './api/fetch/images/'+element.imageName;
 
 		let date = document.createElement('p');
 		date.textContent = element.date;
-
 		container.appendChild(title);
 		container.appendChild(description);
 		container.appendChild(date);
+		container.appendChild(image)
 		// append to fragement to avoid excessive repaints
 		fragment.appendChild(container);
 	});
