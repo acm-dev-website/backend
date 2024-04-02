bd78d3283bbcd001760a7e421822271f8fa118d9
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-04-02 00:51:09 -0400

    file structure rework to support frontend integration

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { cookieAuthCheck } = require("../utils/secureCookie.js");
-const { render, create, deleteEvent } = require('../controllers/adminController.js');
+const { render, create, deleteEvent,login,loginRender } = require('../controllers/adminController.js');
 const bodyParser = require("body-parser");
 const file_utils = require("../utils/file_utils.js");
 
-router.use(['/','submit'], cookieAuthCheck);
+console.log(__dirname);
+//router.use(express.static(__dirname+'../public'))
+
 router.use('/submit', file_utils.upload.single('image'));
 
-router.get('/', render);
-router.post('/submit', create);
-router.post('/delete', deleteEvent);
+router.get('/',  cookieAuthCheck,render);
+router.get('/login',loginRender)
+router.post('/login',login)
+router.post('/submit',  cookieAuthCheck, create);
+router.post('/delete', /*cookieAuthCheck,*/ deleteEvent);
 
 module.exports = router;
