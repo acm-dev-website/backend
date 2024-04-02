12de0e35098dd518c70fc4d09c9dfa8aab8bb750
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-04-02 02:52:24 -0400

    Current version: Search, delete, modals
    Search, delete event, and modals for both delete and edit are now done. Delete handles image deletion perfectly fine. Search has the issues of not showing images and not updating properly once an element is deleted. Implement these issues, comment and clean up code, finish edit, add filtering :DDDDDDDDDD

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,6 @@
 const mongo_utils = require('../utils/mongo_utils');
 const path = require('path');
 
-//const ObjectId = require('mongodb').ObjectID;
 
 // Stuff for gridfsbucket, also set up multer as to use req.file.originalname
 const multer = require("multer");
@@ -66,34 +65,38 @@ exports.deleteEvent = async (req,res)=>{
 
   const eventName = req.query.eventName; // Specify the name of the event to delete
   const currentEventImgName = req.query.imgName;
-  console.log(req.query.eventName);
-  console.log(req.query.imgName);
-  console.log(`Deleting event with name: ${eventName}`);
+  //console.log(req.query.eventName);
+  //console.log(req.query.imgName);
+  //console.log(`Deleting event with name: ${eventName}`);
 
   try {
     const db = mongo_utils.get_client().db();
     
     const imgCollection = db.collection('images.files')
-    const imgId = await imgCollection.findOne({filename : currentEventImgName})._id;
+    const imgId = await imgCollection.findOne({filename : currentEventImgName});
     const imgResult = await imgCollection.deleteOne({ filename : currentEventImgName });
 
     const chunksCollection = db.collection('images.chunks')
-    const chunksResult = await chunksCollection.deleteMany({ files_id : imgId });
+    const chunksResult = await chunksCollection.deleteMany({ files_id : imgId._id });
 
     const collection = db.collection('events');
     const result = await collection.deleteOne({ name : eventName });
     
     console.log(result);
+    console.log(imgResult);
+    console.log(chunksResult);
 
-    if (result.deletedCount === 1 && imgResult.deletedCount == 1) {
-      // Event successfully deleted
+    if (result.deletedCount >= 1 && imgResult.deletedCount >= 1 && chunksResult.deletedCount >= 1){
       res.status(200).json({ message: 'Event deleted successfully' });
+      return;
     } else {
-      // Event not found
-      res.status(404).json({ message: 'Event not found' });
+      res.status(404).json({ message: ':(' });
+      return;
     }
   } catch (err) {
-    // Error handling
     res.status(500).json({ message: err.message });
+    return;
   }
+
+  return;
 }
